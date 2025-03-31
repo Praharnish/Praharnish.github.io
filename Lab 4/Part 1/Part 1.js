3f8433986b4a3a6b78043bcf9a9d19e4e251a6e2
Praharnish <harnishprajapati2906@gmail.com>
2025-03-31 19:27:12 -0400

    Update Part 1.js
    created the random name selector to get the random data from the file and add in to the output string.

--- Lab 4/Part 1/Part 1.js
+++ Lab 4/Part 1/Part 1.js
@@ -23,6 +23,16 @@ const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
+    
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":inserty:", yItem);
+    newStory = newStory.replaceAll(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;

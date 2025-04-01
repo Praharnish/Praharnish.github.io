e157e589d8163ddde3f7b59131e206ea572fa7b7
Praharnish <harnishprajapati2906@gmail.com>
2025-04-01 14:52:56 -0400

    Update main.js
    Declaring the alternative text for each image file

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -17,6 +17,14 @@ const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
+const obj = {
+    "pic1.jpg": "Picture of Human eye",
+    "pic2.jpg": "Picture of something wavy",
+    "pic3.jpg": "Lavender flowers",
+    "pic4.jpg": "Ancient Egypt painting",
+    "pic5.jpg": "Butterfly image"
+}
+
 /* Looping through images */
 
 const newImage = document.createElement('img');

c80a99bb7f23e714225357136ea0c24508bbab91
Praharnish <harnishprajapati2906@gmail.com>
2025-04-11 00:46:57 -0400

    final tuch.
    some modification and the correction.

--- Lab 5/main.js
+++ Lab 5/main.js
@@ -14,15 +14,19 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 commentWrapper.style.display = 'none';
 
 showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+  let isExpanded = showHideBtn.getAttribute('aria-expanded') === 'true';
+
+  if (!isExpanded) {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
+    showHideBtn.setAttribute('aria-expanded', 'true');
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
   }
 };
+
 
 // functionality for adding a new comment via the comments form
 
